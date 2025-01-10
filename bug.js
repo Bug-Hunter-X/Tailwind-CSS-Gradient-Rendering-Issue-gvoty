a5557d0beb7f15a54341cb10c806a11623556ad4
BugHunter-X <askzenai@gmail.com>
2025-01-10 19:36:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// ... your Tailwind CSS code ...
+
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4">
+  <p>Some text here</p>
+</div>
+```
+This code uses the `bg-gradient-to-r` utility class, but the gradient isn't showing up correctly.  The colors seem to be wrong or the gradient is not applied as expected. 
